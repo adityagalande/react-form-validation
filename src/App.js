a2975b4c9a610c4c656a3b9e8db01f54971a7322
Aditya Galande <adityagalande14@gmail.com>
2021-08-06 23:28:34 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import BasicForm from "./components/BasicForm";
 import SimpleInput from "./components/SimpleInput";
 
 function App() {
   return (
     <div className="app">
       <SimpleInput />
+      <BasicForm />
     </div>
   );
 }
